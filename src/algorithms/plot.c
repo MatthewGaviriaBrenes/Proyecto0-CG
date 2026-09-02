08b13fa060225eb8a47716858ff7b86caecf2a4c
MatthewGaviriaBrenes <matthewgaviriab@gmail.com>
2026-09-02 13:06:55 -0600

    Cosas basicas

--- src/algorithms/plot.c
+++ src/algorithms/plot.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <GL/gl.h>
+//#include <stdio.h>
 #include "plot.h"
 
 void plot(int x, int y)
 {
-    printf("(%d, %d)\n", x, y);
+    //printf("(%d, %d)\n", x, y);
     //Test momentaneo
+    glVertex2i(x, y);
 }
--- src/main.c
+++ src/main.c
@@ -1,8 +1,35 @@
-#include "midpoint.h"
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include "algorithms/midpoint.h"
 
-int main(void)
+void draw_scene ();
+
+int main(int argc, char **argv)
 {
-    midpoint(2, 2, 8, 5);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("P0-CG - Midpoint");
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    gluOrtho2D(-0.5, 400.5, -0.5, 400.5);
+    glutDisplayFunc(draw_scene);
+    glutMainLoop();
 
     return 0;
+}
+
+void draw_scene(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0, 1.0, 1.0);
+
+    glBegin(GL_POINTS);
+
+    midpoint(2, 2, 8, 5);
+
+    glEnd();
+
+    glFlush();
 }
